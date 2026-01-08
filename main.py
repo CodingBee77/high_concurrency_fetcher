acca48032eee7ec11dbfb1f363ec6bd6e4c3a337
martyna <codingbee77@tlen.pl>
2026-01-08 13:41:45 +0100

    Add script execution to the main.py file

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
-def print_hi(name):
-    print(f'Hi, {name}')
+import asyncio
+import json
+
+from data_fetcher import DataFetcher
 
 
-if __name__ == '__main__':
-    print_hi('Python'
+async def main():
+    ids_to_fetch = range(1, 51) # Lets fetch the first 50 posts
+    fetcher = DataFetcher(max_concurrent=5)
+
+    try:
+        print(f"Starting data fetch for {len(ids_to_fetch)} posts...")
+        results = await fetcher.run(ids_to_fetch)
+
+        # If we finish the whole process without interruption
+        with open("fetched_posts_log.txt", "w") as log_file:
+            json.dump(([r for r in results if isinstance(r, dict)]), log_file)
+
+    except KeyboardInterrupt:
+        print("\n⚠️ Interrupted! Saving progress...")
+        # In a real app, you'd access the 'results' gathered so far
+        # and write them to a database or file before exiting.
+
+    finally:
+        print("🧹 Cleaning up connections and exiting.")
+
+
+if __name__ == "__main__":
+    # We catch KeyboardInterrupt here because asyncio.run()
+    # propagates it up from the event loop.
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        pass
