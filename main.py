efd84d2554f19a819b172877817ee89f13d0ee63
martyna <codingbee77@tlen.pl>
2026-01-09 13:35:25 +0100

    Adjust main.py script to catch CTRL+C action and save partial results in log file

--- main.py
+++ main.py
@@ -5,29 +5,25 @@ from data_fetcher import DataFetcher
 
 
 async def main():
-    ids_to_fetch = range(1, 51) # Lets fetch the first 50 posts
+    ids_to_fetch = range(1, 101) # Let's fetch the first 50 posts
     fetcher = DataFetcher(max_concurrent=5)
-
+    accumulated_results = []
     try:
         print(f"Starting data fetch for {len(ids_to_fetch)} posts...")
-        results = await fetcher.run(ids_to_fetch)
+        results = await fetcher.run(ids_to_fetch, accumulated_results)
+        print("✅ Finished all requests successfully.")
 
-        # If we finish the whole process without interruption
-        with open("fetched_posts_log.txt", "w") as log_file:
-            json.dump(([r for r in results if isinstance(r, dict)]), log_file)
-
-    except KeyboardInterrupt:
-        print("\n⚠️ Interrupted! Saving progress...")
-        # In a real app, you'd access the 'results' gathered so far
-        # and write them to a database or file before exiting.
+    except (asyncio.CancelledError, KeyboardInterrupt):
+        print(f"\n⚠️ Interrupted! Saving {len(accumulated_results)} posts...")
+        # raise
 
     finally:
+        with open("fetched_posts_log_new.log", "w") as log_file:
+            json.dump(([r for r in accumulated_results if isinstance(r, dict)]), log_file)
         print("🧹 Cleaning up connections and exiting.")
 
 
 if __name__ == "__main__":
-    # We catch KeyboardInterrupt here because asyncio.run()
-    # propagates it up from the event loop.
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
